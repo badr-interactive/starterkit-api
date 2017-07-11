772c5b0a3eddd4c96d0ff307d95b3d1b1fb8b627
Salman El Farisi <salman.farisi@badr-interactive.com>
2017-07-11 15:18:10 +0700

    Refactor auth controller spec
    Refactoring auth controller spec test using kahlan given() method

--- spec/modules/auth/AuthController.spec.php
+++ spec/modules/auth/AuthController.spec.php
@@ -1,25 +1,36 @@
 <?php
 
 describe('Auth Controller', function() {
+    given('request', function() {
+        $environment = \Slim\Http\Environment::mock([]);
+        $request = \Slim\Http\Request::createFromEnvironment($environment);
+        $requestBody = $request->getBody();
+        $requestBody->write(json_encode(['name' => 'salman']));
+        $request->withBody($requestBody);
+
+        return $request;
+    });
+
+    given('response', function() {
+        return new \Slim\Http\Response();
+    });
+    
     describe('register()', function() {
-        it('returns valid json string', function() {
-            $environment = \Slim\Http\Environment::mock([
-                'REQUEST_METHOD' => 'POST',
-                'REQUEST_URI' => '/auth/request',
-                'CONTENT_TYPE' => 'application/json',
-            ]);
-
-            $response = new \Slim\Http\Response();
-            $request = \Slim\Http\Request::createFromEnvironment($environment);
-            $requestBody = $request->getBody();
-            $requestBody->write(json_encode(['name' => 'salman']));
-
+        it('should returns valid json string', function() {
             $app = new \App\Modules\Auth\AuthController();
-            $result = $app->register($request, $response);
+            $result = $app->register($this->request, $this->response);
             json_decode((string)$result->getBody());
 
             expect($result->getHeader('Content-Type')[0])->toBe('application/json;charset=utf-8');
             expect(json_last_error() == JSON_ERROR_NONE)->toBe(true);
+        });
+
+        it('should returns HTTP Code 200', function() {
+            $app = new \App\Modules\Auth\AuthController();
+            $result = $app->register($this->request, $this->response);
+            json_decode((string)$result->getBody());
+
+            expect($result->getStatusCode())->toBe(200);
         });
     });
 });
