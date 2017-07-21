b78f892d05abd68adc8b5b66a9bda5884b145236
Salman El Farisi <salman.farisi@badr-interactive.com>
2017-07-21 09:29:28 +0700

    Cleaning up :)

--- spec/modules/auth/UserLogin.spec.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-describe('AuthController', function() {
-    
-});
--- src/modules/auth/AuthController.php
+++ src/modules/auth/AuthController.php
@@ -48,21 +48,6 @@ class AuthController
         return $response->withJson($responseData, 200);
     }
 
-    public function login(Request $request, Response $response)
-    {
-        $contentType = $request->getHeaderLine('Content-Type');
-        if($contentType !== 'application/json') {
-            return $response->withJson(['success' => false], 400);
-        }
-
-        $params = $request->getParsedBody();
-        $checklist = ['email', 'password', 'device_id', 'fcm_token'];
-        if(!$this->validateRequiredParam($checklist, $request)) {
-            return $response->withJson(['success' => false], 401);
-        }
-        return $response->withJson(['success' => true], 200);
-    }
-
     private function validateRequiredParam($checklist = [], $request)
     {
         $params = $request->getParsedBody();
