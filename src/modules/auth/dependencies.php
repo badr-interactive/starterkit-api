ea9a0ffadf8bd4597a0bfe9a38fc4a0c038a5e1f
Salman El Farisi <salman.farisi@badr-interactive.com>
2017-07-27 15:00:54 +0700

    temporary remove directory

--- src/modules/auth/dependencies.php
+++
@@ -1,8 +0,0 @@
-<?php
-$container['User'] = function($c) {
-    return new App\Modules\Auth\Model\User;
-};
-
-$container['UserQuery'] = function($c) {
-    return new App\Modules\Auth\Model\UserQuery;
-};
